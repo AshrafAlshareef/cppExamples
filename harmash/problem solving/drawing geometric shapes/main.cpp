b8611c1e65c7deb0d9510163524b3296cb8c52cd
Ashraf Alshareef <44895610+AshrafAlshareef@users.noreply.github.com>
2024-12-23 16:07:52 +0100

    Update main.cpp

--- harmash/problem solving/drawing geometric shapes/main.cpp
+++ harmash/problem solving/drawing geometric shapes/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include "geometric_shapes_drawing_1.h"
 #include "geometric_shapes_drawing_2.h"
+#include "geometric_shapes_drawing_3.h"
 
 // test for geometric_shapes_drawing_1
-// int main()
-// {
-//     int lineNumbers = 5; 
-//     drawLeftAlignedIncreasingStars(lineNumbers);
-//     drawLeftAlignedDecreasingStars(lineNumbers);
-//     drawRightAlignedIncreasingStars(lineNumbers);
-//     drawRightAlignedDecreasingStars(lineNumbers);
-//     drawColumnStars(lineNumbers, 2 /*Columns*/);
-//     system("pause");
-//     return 0;
-// }
+int main()
+{
+    int lineNumbers = 5; 
+    geometric_shapes_drawing_1::drawLeftAlignedIncreasingStars(lineNumbers);
+    geometric_shapes_drawing_1::drawLeftAlignedDecreasingStars(lineNumbers);
+    geometric_shapes_drawing_1::drawRightAlignedIncreasingStars(lineNumbers);
+    geometric_shapes_drawing_1::drawRightAlignedDecreasingStars(lineNumbers);
+    geometric_shapes_drawing_1::drawColumnStars(lineNumbers, 2 /*Columns*/);
+    system("pause");
+    return 0;
+}
 
 
 // test for geometric_shapes_drawing_2
@@ -25,6 +27,18 @@ int main()
     geometric_shapes_drawing_2::drawReversedTriangle(lineNumbers);
     geometric_shapes_drawing_2::drawRightHeadTriangle(lineNumbers);
     geometric_shapes_drawing_2::drawLeftHeadTriangle(lineNumbers);
+    system("pause");
+    return 0;
+}
+
+// test for geometric_shapes_drawing_3
+int main()
+{
+    int lineNumbers = 5;
+    int numberStars = 5;
+    geometric_shapes_drawing_3::drawEmptyRhombusShape(lineNumbers);
+    geometric_shapes_drawing_3::drawEmptyRhombusShape(lineNumbers, "-");
+    geometric_shapes_drawing_3::drawRectangle(7, 15);
     system("pause");
     return 0;
 }
